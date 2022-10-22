4ebe378fc9c093c3737a47ef5be0514335c11c89
Raihan Khan <34377027+raihankhan-rk@users.noreply.github.com>
2022-10-22 17:06:36 +0530

    TwoSum Leetcode

---
+++ Leetcode/TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> mp;
+
+        for(int i=0; i<(int)nums.size(); i++)
+        {
+            if(mp.count(target - nums[i]))
+                return {i, mp[target-nums[i]]};
+            
+            mp[nums[i]] = i;
+        }
+        return {};
+        
+    }
+};
